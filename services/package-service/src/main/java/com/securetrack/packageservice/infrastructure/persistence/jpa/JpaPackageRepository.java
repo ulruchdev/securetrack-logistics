d70375bf4d36827fc155a2c8a577adbe057c60c6
ulrich dev <ulruchdev@gmail.com>
2026-04-02 18:26:00 +0200

    Revert "chore: merge code review fixes into develop"
    This reverts commit 20720d5b1c94c8cd4538e1d799151c25f48be06f, reversing
    changes made to f229e711c3c7b5612fe9e932eb54fc1588eb13b1.

--- services/package-service/src/main/java/com/securetrack/packageservice/infrastructure/persistence/jpa/JpaPackageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.securetrack.packageservice.infrastructure.persistence.jpa;
-
-import com.securetrack.packageservice.infrastructure.persistence.entity.JpaPackageEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface JpaPackageRepository extends JpaRepository<JpaPackageEntity, String> {
-}
